b6888cb1987e72633113c67c8245d03bac8d8041
JohnL <jlamertina@francis.edu>
2015-10-05 13:13:15 -0400

    added routes for integer, float, and path entries

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ def hello_world():
 @app.route("/test/<search_query>")
 def search(search_query):
 	return search_query
+
+# dyanmic routes that accept int, float, and file path
+@app.route("/int_test/<int:value>")
+def int_type(value):
+	print ("Int: incrementing {} is {}".format(value, value+1))   # print to console
+	return "correct"
+@app.route("/float_test/<float:entry>")
+def float_type(entry):
+	print ("Float: incrementing {} is {}".format(entry, entry+1))
+	return "correct"
+@app.route("/path_test/<path:input>")
+def path_type(input):
+	print ("Path: input path is {}".format(input))
+	return "correct"
+
 if __name__ =="__main__":
     app.run()
 # run this file: python app.py
